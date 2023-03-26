9229cdd4f19bfc60e3396fbd1d83475da055391b
hv <hv@PM309-01>
2023-03-26 09:52:11 +0700

    Add project files.

---
+++ Buoi4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so nguyen n= ");
+            int n = int.Parse(Console.ReadLine());
+            int tong = 0;
+            int gt = 1;
+            for  (int i =1; i <= n; i++) 
+            {
+                    gt = gt * i;
+                    tong = tong + gt;
+            }
+            Console.WriteLine($"Tong giai thua tu 1 den {n} la {tong}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Buoi4_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap vao so nguyen n = ");
+            int n =int.Parse(Console.ReadLine());
+            int dem_so=0;
+            int tong = 0;
+            for (int i = 2; i <= n; i++)
+            {
+                dem_so = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0) 
+                    {
+                        dem_so ++;
+                    }
+                }
+                if (dem_so == 2)
+                {
+                    tong=tong+i;
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine($"Tong cac so nguyen to tu 2 den {n} la {tong}");
+            Console.ReadLine() ;    
+        }
+    }
+}
